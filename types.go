2fe97c6f1e50da1ac1be7d24477ace983e00f312
wangchonglin <735761545@qq.com>
2020-07-24 13:54:48 +0800

    简体繁体中文转换前缀树

---
+++ types.go
@@ -0,0 +1,11 @@
+package prefixtree-opencc
+
+import (
+	core "github.com/ApesPlan/prefixtree-core"
+)
+
+// Dict contains the Trie and dict values
+type Dict struct {
+	Trie   *core.PrefixTree
+	Values [][]string
+}
